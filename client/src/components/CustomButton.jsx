b22dc976c6b5fea79d21253e4b30695b51442c37
Thibault <tweb.devc@gmail.com>
2023-07-27 17:46:07 +0200

    frontend done

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import state from '../store'
 import { useSnapshot } from 'valtio'
+import { getContrastingColor } from '../config/helpers'
 
 const CustomButton = ({type, customStyles, handleClick, title}) => {
 
@@ -10,9 +11,15 @@ const CustomButton = ({type, customStyles, handleClick, title}) => {
        if(type === 'filled') {
         return {
             backgroundColor: snap.color,
-            color: '#fff'
+            color: getContrastingColor(snap.color)
         }
-       } 
+       } else if (type=== "outline") {
+        return {
+          borderWidth: '1px',
+          borderColor: snap.color,
+          color: snap.color
+        }
+       }
     }
 
   return (
